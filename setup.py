429fa4adc30e864423957d7cc5357369348477a7
rito <pellegrino.arthur@gmail.com>
2022-10-04 14:22:06 +0200

    all module imports added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='hand-decoding',
+    name='hand_decoding',
     url='https://github.com/arthur-pe/hand-decoding.git',
     author='arthur',
     #author_email='',
